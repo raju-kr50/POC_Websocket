2ba6d9d5d41223a8b752646557c51efabf191658
raju.kumar <raju.kumar@cgi.com>
2019-08-20 07:25:49 +0530

    Completed web-socket

---
+++ websocket-angular/src/app/app.component.ts
@@ -0,0 +1,38 @@
+import { Component } from '@angular/core';
+import * as Stomp from 'stompjs';
+import * as SockJS from 'sockjs-client';
+import $ from 'jquery';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Welcome to websocket chat';
+  private serverUrl = 'http://localhost:8085/message';
+  private stompClient;
+  constructor() {
+    this.initializeWebSocketConnection();
+  }
+
+  initializeWebSocketConnection() {
+    const websocket = new SockJS(this.serverUrl);
+    this.stompClient = Stomp.over(websocket);
+    const that = this;
+    // tslint:disable-next-line:only-arrow-functions
+    this.stompClient.connect({}, function(frame) {
+      that.stompClient.subscribe('/chat', (message) => {
+        if (message.body) {
+          $('.chat').append('<div class=\'message\'>' + message.body + '</div>');
+          console.log(message.body);
+        }
+      });
+    });
+  }
+
+  sendMessage(message) {
+    this.stompClient.send('/app/send/message' , {}, message);
+    $('#input').val('');
+  }
+}
